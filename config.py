6b2680b9bec268454fe6858099df07d592d2d81f
Rahulmeghna21 <rahulsk8313@gmail.com>
2024-12-14 17:11:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8112795608:AAGVJMRik8S9XU2C5Ho2SLzIwSwXeGWHphs")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAHUL_DDOS_B")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1661744209")
+
+
+
+
+
+
+
+  
